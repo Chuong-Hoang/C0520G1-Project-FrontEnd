5df31942894b824fca8ad4304e488e3e9760fd0c
letoandn2192 <talaquanlaogia123@gmail.com>
2020-11-19 09:30:01 +0700

    common and login function second

---
+++ meeting-room-management/src/app/office-common/component/login/login.component.ts
@@ -0,0 +1,52 @@
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {AuthenticationService} from '../../service/auth/authentication.service';
+import {TokenStorageService} from '../../service/token-storage/token-storage.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  form: any = {};
+  isLoggedIn = false;
+  isLoginFailed = false;
+  errorMessage = '';
+  role: string;
+
+  constructor(private authenticationService: AuthenticationService, private tokenStorage: TokenStorageService,
+              private router: Router) {
+  }
+
+  ngOnInit(): void {
+    if (this.tokenStorage.getToken()) {
+      this.isLoggedIn = true;
+      this.role = this.tokenStorage.getUser().role;
+    }
+  }
+
+  onSubmit(): void {
+    this.authenticationService.login(this.form).subscribe(
+      data => {
+        this.tokenStorage.saveToken(data.accessToken);
+        this.tokenStorage.saveUser(data);
+
+        this.isLoginFailed = false;
+        this.isLoggedIn = true;
+        this.reloadPage();
+        // this.router.navigate(['/']);
+      },
+      err => {
+        this.errorMessage = err.error.message;
+        this.isLoginFailed = true;
+      }
+    );
+  }
+
+  reloadPage(): void {
+    window.location.reload();
+  }
+
+}
