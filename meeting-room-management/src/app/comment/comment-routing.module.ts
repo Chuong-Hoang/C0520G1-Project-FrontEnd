5e6009f2a88789c9ca235886ccbd9298dba68c1a
nhatle352840 <leenhat11a1nh@gmail.com>
2020-11-19 09:09:40 +0700

    nhat commit angular 2

--- meeting-room-management/src/app/comment/comment-routing.module.ts
+++ meeting-room-management/src/app/comment/comment-routing.module.ts
@@ -1,8 +1,8 @@
 import {Routes} from '@angular/router';
-import {CommentListComponent} from './comment-list/comment-list.component';
-import {CommentCreateComponent} from './comment-create/comment-create.component';
-import {CommentHandleComponent} from './comment-handle/comment-handle.component';
-import {NotificationComponent} from './notification/notification.component';
+import {CommentListComponent} from './component/comment-list/comment-list.component';
+import {CommentCreateComponent} from './component/comment-create/comment-create.component';
+import {CommentHandleComponent} from './component/comment-handle/comment-handle.component';
+import {NotificationComponent} from './component/notification/notification.component';
 
 export const CommentRoutes: Routes = [
   {
--- meeting-room-management/src/app/comment/comment.module.ts
+++ meeting-room-management/src/app/comment/comment.module.ts
@@ -1,12 +1,12 @@
 import {CUSTOM_ELEMENTS_SCHEMA, NgModule, NO_ERRORS_SCHEMA} from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CommentListComponent } from './comment-list/comment-list.component';
+import { CommentListComponent } from './component/comment-list/comment-list.component';
 import {CommentRoutes} from './comment-routing.module';
 import {RouterModule} from '@angular/router';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { CommentHandleComponent } from './comment-handle/comment-handle.component';
-import { CommentCreateComponent } from './comment-create/comment-create.component';
-import { NotificationComponent } from './notification/notification.component';
+import { CommentHandleComponent } from './component/comment-handle/comment-handle.component';
+import { CommentCreateComponent } from './component/comment-create/comment-create.component';
+import { NotificationComponent } from './component/notification/notification.component';
 
 
 @NgModule({
