738556cf1714af61e7871bd2e9874505df100482
nguyentienhai130801 <tienhai19631959@gmail.com>
2020-11-19 08:32:44 +0700

    hai update statistic

---
+++ meeting-room-management/src/app/statistic-room/statistic-room.routers.ts
@@ -0,0 +1,8 @@
+import {Routes} from '@angular/router';
+import {ViewStatisticComponent} from './view-statistic/view-statistic.component';
+import {StatisticalResultsByRoomComponent} from './statistical-results-by-room/statistical-results-by-room.component';
+
+export const statisticRoomRoutes: Routes = [
+  { path: '',
+    component: ViewStatisticComponent}
+];
