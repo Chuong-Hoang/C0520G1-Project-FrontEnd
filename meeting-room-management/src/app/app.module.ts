e826cb5a06bd02c6ae5d4f16297ec9e1d2680649
nhatle352840 <leenhat11a1nh@gmail.com>
2020-11-20 11:51:11 +0700

    nhat commit angular

--- meeting-room-management/src/app/app.module.ts
+++ meeting-room-management/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {NgxPaginationModule} from 'ngx-pagination';
-import {MaterialModule} from './material.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {BookedRoomModule} from './booked-room/booked-room.module';
@@ -20,7 +19,6 @@ import {HttpClientModule} from '@angular/common/http';
   ],
   imports: [
     BrowserModule,
-    MaterialModule,
     NgxPaginationModule,
     BookedRoomModule,
     CommentModule,
