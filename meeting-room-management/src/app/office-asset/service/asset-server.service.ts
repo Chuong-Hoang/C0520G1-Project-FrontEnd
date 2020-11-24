603e0716e53704807a5e04f317ddb018ade5236d
tungtyu1995 <tusytung@gmail.com>
2020-11-24 22:07:28 +0700

    tung commit backend 24/11 22h

--- meeting-room-management/src/app/office-asset/service/asset-server.service.ts
+++ meeting-room-management/src/app/office-asset/service/asset-server.service.ts
@@ -7,43 +7,45 @@ import {Asset} from '../model/model.asset';
   providedIn: 'root'
 })
 export class AssetServerService {
-  public Api = 'http://localhost:8080/assets';
+  public API = 'http://localhost:8080/assets';
 
   constructor(
     public http: HttpClient
   ) {
   }
 
-
+  // get all assets
   getAll(): Observable<Asset> {
-    return this.http.get<Asset>(this.Api);
+    return this.http.get<Asset>(this.API);
   }
 
+  // get asset by id
   getByID(idAsset): Observable<Asset> {
     console.log(idAsset);
-    return this.http.get<Asset>(this.Api + '/detail/' + idAsset);
+    return this.http.get<Asset>(this.API + '/detail/' + idAsset);
   }
 
+  // create asset
   create(asset): Observable<any> {
     console.log('service' + asset);
-    return this.http.post(this.Api + '/create', asset);
+    return this.http.post(this.API + '/create', asset);
   }
 
+  // edit asset by idAsset
   edit(asset, assetId): Observable<Asset> {
-    return this.http.patch<Asset>(this.Api + '/edit/' + assetId, asset);
+    return this.http.patch<Asset>(this.API + '/edit/' + assetId, asset);
   }
 
+  // delete asset by idAsset
   delete(assetId): Observable<Asset> {
-    return this.http.delete<Asset>(this.Api + '/delete/' + assetId);
+    return this.http.delete<Asset>(this.API + '/delete/' + assetId);
   }
 
-
+  // search assets by nameAsset containing
   searchByNameAsset(inputSearch: string): Observable<Asset> {
-
     let params = new HttpParams();
     params = params.append('valueSearch', inputSearch);
-
-    return this.http.get<Asset>(this.Api + '/inputSearch' , {params});
+    return this.http.get<Asset>(this.API + '/inputSearch' , {params});
   }
 
 }
