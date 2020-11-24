7cb7c17368d66f2aebeef0528c9c69ac9485431f
tea <sontra140395@gmail.com>
2020-11-24 08:49:58 +0700

    Son Tra commit font-end

---
+++ meeting-room-management/src/app/assets-detail/component/assets-detail-dialog-choice/assets-detail-dialog-choice.component.ts
@@ -0,0 +1,24 @@
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import {AssetsDetailService} from '../../service/assets-detail.service';
+import {AssetDetail} from '../../model/AssetDetail.class';
+
+@Component({
+  selector: 'app-assets-detail-dialog-choice',
+  templateUrl: './assets-detail-dialog-choice.component.html',
+  styleUrls: ['./assets-detail-dialog-choice.component.css']
+})
+export class AssetsDetailDialogChoiceComponent implements OnInit {
+  public assetsDetailsList;
+  public idAsset;
+  constructor(
+    public dialogRef: MatDialogRef<AssetsDetailDialogChoiceComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+) { }
+
+  ngOnInit(): void {
+    this.assetsDetailsList = this.data.data1;
+    this.idAsset = this.data.data1.idAsset;
+  }
+
+}
---
+++ meeting-room-management/src/app/assets-detail/service/assets-detail.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import { Observable} from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AssetsDetailService {
+  public API_ASSETS_DETAIL = 'http://localhost:8080/assets';
+
+  constructor(
+  public http: HttpClient
+  ) { }
+
+  getAssetsDetailById(idAssetsDetail): Observable<any> {
+    return this.http.get(this.API_ASSETS_DETAIL + '/' + idAssetsDetail);
+  }
+  getAllAssetsDetail(): Observable<any> {
+    return this.http.get(this.API_ASSETS_DETAIL);
+  }
+
+}
