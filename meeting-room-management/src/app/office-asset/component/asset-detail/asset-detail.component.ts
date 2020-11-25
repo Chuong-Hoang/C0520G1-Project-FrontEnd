b7f1348c9bbc68a61429b93b41e6044838ee4ffe
letoandn2192 <talaquanlaogia123@gmail.com>
2020-11-25 23:21:53 +0700

    test code

--- meeting-room-management/src/app/office-asset/component/asset-detail/asset-detail.component.ts
+++ meeting-room-management/src/app/office-asset/component/asset-detail/asset-detail.component.ts
@@ -23,7 +23,7 @@ export class AssetDetailComponent implements OnInit {
     const id = +this.route.snapshot.paramMap.get('id');
     this.assetService.getByID(id).subscribe(data => {
       this.asset = data;
-      data.image = data.image.substring(11);
+      data.image = data.image.substring(10);
       console.log(this.asset);
     } , error => console.log('error'));
   }
